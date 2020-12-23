dcd6f96267b58aef8301a61362ffd3e2dcb815f1
Scott Landes <scottandvega@gmail.com>
2020-12-22 21:34:19 -0800

    installed pundit gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username])
   end
+
+  include Pundit
+
 end
